34005f4a07d5fd2dfec0e241c388bb259936adce
gborie <gborie7@gmail.com>
2018-07-05 15:07:47 +0100

    do while loop

--- CS_Masterclass5/Program.cs
+++ CS_Masterclass5/Program.cs
@@ -10,10 +10,14 @@ namespace CS_Masterclass5
     {
         static void Main(string[] args)
         {
-            for(int counter = 1; counter < 20; counter += 2)
+            int counter = 0;
+            do
             {
                 Console.WriteLine(counter);
+                counter++;
             }
+            while (counter < 10);
+
             Console.Read();
         }
     }
